58e80746e9bf1f509b21b7ba7b9bb1133edead7e
= <tanmay@LAPTOP-HONOU8G3>
2023-10-25 18:55:26 -0700

    Overwriting Mandar's code

--- p2/main.c
+++ p2/main.c
@@ -7,6 +7,61 @@
  * main.c
  */
 
+// Memory Allocation Code
+
+// -S flag tells compiler to generate Assembly code for current execution
+// -o<level>, for e.g. -o3, modifies Assembly code according to the level as well
+// Stack usage can be overridden and does not need to be preallocated. So, concurrent stack sharing cannot easily be done.
+// Data cannot move into special registers like Stack Pointer, Program Counter, etc. if it does not exist in a general purpose register
+
+// #define SZ_STACK 4096
+
+// void fnc(void) {
+// 	uint64_t rsp;
+// 	rsp = 17;
+// 	// rsp = (uint64_t)thread->stack.memory + SZ_STACK;
+// 	__asm__ volatile ("mov %[rs], %%rsp \n" : [rs] "+r" [rsp] ::);
+// }
+
+// Scheduler function
+
+// u_int64 scheduler_create(fnc, arg) {
+// 	page_size x;
+// 	allocate thread object
+// 	initialize thread object
+// 	link to global state variable
+// 	*fnc.arg;
+// 	allocate stack
+// 	link it to state.head
+//  return 0;
+// }
+
+// void scheduler_execute(void) {
+// 	setjmp(checkpoint) -> state.ctx;
+// 	schedule();
+// 	destroy();
+// }
+
+// void scheduler_yield() {
+// 	setjmp(checkpoint) -> state.thread -> ctx;
+// 	// option 1- if thread is stll valid
+// 	state.thread -> status = SLEEPING;
+// 	longjmp(state.ctx);
+// 	// option 2- if thread execution is completed or thread is invalid
+// 	return 
+// }
+
+// void schedule(void) {
+// 	get candidate from thread_candidate
+// 	// option 1- no candidate is returned
+// 	return
+// 	// option 2- valid candidate is returnded
+// 	state.thread = thread
+// 	// not status_??
+// 	thread.status = RUNNING;
+// 	longjmp(thread->ctx);
+// }
+
 #include "system.h"
 #include "scheduler.h"
 
--- p2/scheduler.c
+++ p2/scheduler.c
@@ -22,3 +22,63 @@
  */
 
 /* research the above Needed API and design accordingly */
+
+#define SZ_STACK 4096
+
+typedef struct thread {
+    jmp_buf ctx;
+    enum {
+        STATUS_,
+        STATUS_RUNNING,
+        STATUS_SLEEPING,
+        STATUS_TERMINATED
+    } status;
+    struct {
+        void *memory_;
+        void *memory;
+    } stack;
+    struct thread *link;
+    scheduler_fnc_t fnc;
+    const char *name;
+} Thread;
+
+static struct {
+    Thread *head;
+    Thread *cur_thread;
+    jmp_buf ctx;
+} state;
+
+Thread *thread_candidate(void) {
+
+}
+
+int scheduler_create(scheduler_fnc_t fnc, void *arg) {
+    size_t page_size_ = page_size();
+    int num_pages;
+
+    Thread *thread = (Thread *)malloc(sizeof(Thread));
+    /* Error handling for memory allocation failure */
+    if (!thread) {
+        fprintf(stderr, "Memory allocation failed for thread.\n");
+        return -1;
+    }
+
+    thread->name = (const char *)arg;
+    thread->status = STATUS_;
+    
+    thread->fnc = fnc;
+    thread->stack.memory_ = malloc(SZ_STACK);
+    /* Error handling for stack allocation failure */
+    if (!thread->stack.memory_) {
+        fprintf(stderr, "Stack allocation failed for stack.\n");
+        return -1;
+    }
+
+    num_pages = (int)SZ_STACK/(int)page_size_ + ((int)SZ_STACK % (int)page_size_ != 0) + 1;
+
+    thread->stack.memory = memory_align(thread->stack.memory_, (size_t)num_pages);
+    thread->link = state.head;
+    state.head = thread;
+    
+    return 0;
+}
